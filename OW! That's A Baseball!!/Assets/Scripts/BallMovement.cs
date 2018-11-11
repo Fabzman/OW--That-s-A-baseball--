9ba79e5e880597aec4ccf330bbc68674f541fc59
Fabzman <Fabian.Est.Figueroa@gmail.com>
2018-11-11 20:00:29 +1100

    Gameobjects, Rotation, Spawner
    Created Baseball Field gameobjects, bases, pitchers mound, batters mound etc. Created Player and baseball bat gameobjects, created pitching machine gameobject along with spawner gameobject, created baseball game object that spawns. Created Ball Spawning script from pitching machine, created ballswing script that rotates the player gameobject when pressing space.

---
+++ OW! That's A Baseball!!/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    public float ballSpeed;
+    public float lifetime;
+    //public Transform player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //player = GameObject.Find("Player").transform;
+        lifetime = 5f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * ballSpeed * Time.deltaTime);
+        lifetime -= Time.deltaTime;
+
+        if (lifetime <=0)
+        {
+            lifetime = 5f;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ OW! That's A Baseball!!/Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+
+    public static BallSpawn instance;
+
+    public int ballCount;
+    public float spawnTimer;
+    public GameObject ball;
+    //public Transform ballSpawner;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnTimer = 2f;
+        ballCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (ballCount >= 1)
+        {
+            return;
+        }
+
+        if (spawnTimer <= 0 && ballCount < 1)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+            ballCount++;
+            spawnTimer = 5f;
+        }
+    }
+}
